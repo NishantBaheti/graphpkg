15c87803054bfc756f4a441f82dc36dc34ed0b14
Nishant Baheti <nishantbaheti.it19@gmail.com>
2021-03-08 01:34:27 +0530

    progress commit

--- source/conf.py
+++ source/conf.py
@@ -31,14 +31,17 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
     'sphinx_rtd_theme',
-    'sphinx.ext.viewcode',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+source_suffix = '.rst'
+
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -51,14 +54,8 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints', '*.ipynb','__pycache__']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-## read the docs
-# html_theme = 'sphinx_rtd_theme'
-
-# html_theme = "groundwork"
-
-extensions.append("faculty_sphinx_theme")
-html_theme = "faculty-sphinx-theme"
-
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
