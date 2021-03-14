d99f35e255ae1fd70cfb5e5efc73b438f5bf1932
Nishant Baheti <nishantbaheti.it19@gmail.com>
2021-03-14 16:43:54 +0530

    progress commit

--- src/graphpkg/__init__.py
+++ src/graphpkg/__init__.py
@@ -7,7 +7,6 @@ else:
     from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
-    # Change here if project is renamed and does not equal the package name
     dist_name = __name__
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
